91a1fe8c8abfaf7c7a6a3290dc8a8d892495bdfc
Nikolay Sheregeda <ns.sheregeda@gmail.com>
2016-04-07 22:48:29 +0300

    prepare package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'smsaero',
+    version = '0.1',
+    description = u'send SMS via smsaero.ru',
+    author = 'Nikolay Sheregeda',
+    author_email = 'ns.sheregeda@gmail.com',
+    url = 'https://github.com/sheregeda/smsaero',
+    license = 'MIT',
+    packages = ['smsaero'],
+    zip_safe = False,
+    keywords = ['sms', 'sending'],
+)
