d7b6b1325ec7f211eff7a6fef313ea712410068e
Nikolay Sheregeda <ns.sheregeda@gmail.com>
2016-04-09 19:35:34 +0300

    add tests folder and travis config

---
+++ tests/test_api.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+import unittest
+#from smsaero import *
+
+
+class TestApi(unittest.TestCase):
+
+    def test_some_request(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
